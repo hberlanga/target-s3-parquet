e621173ff0bc5eea655f340359e683596bfd0f30
Hector Berlanga <hector.berlanga@myinvestor.es>
2024-08-07 18:00:25 +0200

    TEst

--- target_s3_parquet/sinks.py
+++ target_s3_parquet/sinks.py
@@ -62,7 +62,7 @@ class S3ParquetSink(BatchSink):
 
         df["_sdc_started_at"] = STARTED_AT.timestamp()
 
-        self.logger.info(f'Ejemplo de fecha => {df["FECHA_MOD_CAPITALIZACION"][:1][0]}')
+        self.logger.info(f'Ejemplo de fecha => {df["FECHA_MOD_CAPITALIZACION"].dtype}')
 
         current_schema = generate_current_target_schema(self._get_glue_schema())
         tap_schema = generate_tap_schema(
