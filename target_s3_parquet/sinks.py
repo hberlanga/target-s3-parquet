f95121070cbde1b90acaeb08a0eb4c80544e330d
Hector Berlanga <hector.berlanga@myinvestor.es>
2024-08-09 09:25:27 +0200

    remove wrong dates

--- target_s3_parquet/sinks.py
+++ target_s3_parquet/sinks.py
@@ -70,6 +70,7 @@ class S3ParquetSink(BatchSink):
 
         for k,v in tap_schema.items():
             if v == 'timestamp':
+                df[k].apply(lambda x: x if 1975 <= x.year < 2262 else None)
                 df[k] = pd.to_datetime(df[k], utc=True)
 
         dtype = {**current_schema, **tap_schema}
